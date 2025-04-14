da0f52521617d7c75be67106a5a91e555962bc75
shahriar021 <shahriarc19@gmail.com>
2025-04-14 22:06:23 +0600

    feat:routing with express

--- app.js
+++ app.js
@@ -2,6 +2,16 @@ const express = require("express");
 
 const app = express();
 
+// routing is very very important...................
+app.get("/yoo/23/:userId", (req, res) => {
+  console.log(req.params);
+  res.send("yoo yoo from the 23");
+});
+
+app.use("/yoo", (req, res) => {
+  res.send("yoo yoo from the server...");
+});
+
 app.use("/", (req, res) => {
   res.send("from the server");
 });
