622992b636979a9752cb811f5de5cd48b9d81f89
shahriar021 <shahriarc19@gmail.com>
2025-04-19 20:38:44 +0600

    multiple route handle

--- app.js
+++ app.js
@@ -3,18 +3,29 @@ const express = require("express");
 const app = express();
 
 // routing is very very important...................
-app.get("/yoo/23/:userId", (req, res) => {
-  console.log(req.params);
-  res.send("yoo yoo from the 23");
-});
+// app.get("/yoo/23/:userId", (req, res) => {
+//   console.log(req.params);
+//   res.send("yoo yoo from the 23");
+// });
 
-app.use("/yoo", (req, res) => {
-  res.send("yoo yoo from the server...");
-});
-
-app.use("/", (req, res) => {
-  res.send("from the server");
-});
+app.use(
+  "/yoo",
+  (req, res, next) => {
+    console.log("route handle 1");
+    res.send("yoo yoo from the server...");
+    next();
+  },
+  [
+    (req, res, next) => {
+      console.log("route handle 2 ");
+      res.send("yoo from 2");
+      next();
+    },
+    (req, res, next) => {
+      console.log("yoo from 3");
+    },
+  ]
+);
 
 app.listen(4000, () => {
   console.log("server is connected...");
